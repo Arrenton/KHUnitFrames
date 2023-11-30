d5a00d7c865266c648b19732d2289a0acd7eccb9
Ryan <gotest512@gmail.com>
2023-11-30 16:41:59 -0700

    Error fix

--- modules/kh_target.lua
+++ modules/kh_target.lua
@@ -454,7 +454,7 @@ function KH_UI:New_KHTargetUnitframe(unit, setting)
                         end
                     end
 
-                    if (UnitExists(self.unit) and not IsReplacingUnit()) then
+                    if (UnitExists(self.unit) and not C_PlayerInteractionManager.IsReplacingUnit()) then
                         if (UnitIsEnemy(self.unit, "player")) then
                             PlaySound(SOUNDKIT.IG_CREATURE_AGGRO_SELECT)
                         elseif (UnitIsFriend("player", self.unit)) then
